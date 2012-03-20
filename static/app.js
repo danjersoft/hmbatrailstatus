e5e81194f9a8f325c8d045b1aaa2fbadb93c9388
unknown <gerritsj@.simon.com>
2012-03-20 14:34:19 -0400

    better labeling

--- static/app.js
+++ static/app.js
@@ -1,16 +1,56 @@
 $('#main').live('pagebeforecreate', function(e) {
+   function getTimeString(time) {
+      var newTime = Math.round(time * 0.001), day = 86400, days, hour = 3600, hours, label = 'Reported ';
+      if (newTime > day) {
+         days = Math.floor(newTime / day);
+         label += days;
+         if (days > 1) {
+            label += ' days'
+         } else {
+            label += ' day'
+         }
+      } else if (newTime > hour) {
+         hours = Math.floor(newTime / hour);
+         label += hours;
+         if (hours > 1) {
+            label += ' hours';
+         } else {
+            label += ' hour';
+         }
+      } else {
+         label += 'less than an hour';
+      }
+      return label + ' ago';
+   }
    $.get('/trailstatus/rss.php', function(data) {
-      var $list = $('#main ul'), li;
+      var $list = $('#main ul'), li, trails = [], now = new Date(), trail;
       $(data).find('item').each(function(index, item) {
-         //console.log($($(item).find('description').text()).text());
+         trails[index] = {
+            "imgUrl": $(item).find('media\\:thumbnail').attr('url'),
+            "title": $(item).find('title').text(),
+            "description": $($(item).find('description').text()).text(),
+            "pubDate": new Date($(item).find('pubDate').text())
+         };
+      });
+      trails.sort(function(a, b) {
+         if (a.title.toLowerCase() < b.title.toLowerCase()) {
+            return -1
+         } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+            return 1;
+         } else {
+            return 0;
+         }
+      });
+      for (var i = 0; i < trails.length; i++) {
+         trail = trails[i];
          li = "<li>";
-         li += '<img src="' + $(item).find('media\\:thumbnail').attr('url') + '" class="ui-li-icon">';
-         li += '<h3>' + $(item).find('title').text() + '</h3>';
-         li += '<p class="ul-li-desc"><strong>' + $($(item).find('description').text()).text() + '</strong></p>';
-         li += '<p class="ul-li-desc">' + $(item).find('pubDate').text() + '</p>';
+         li += '<img src="' + trail.imgUrl + '" class="ui-li-icon">';
+         li += '<h3>' + trail.title + '</h3>';
+         li += '<p class="ul-li-desc"><strong>' + trail.description + '</strong></p>';
+         li += '<p class="ul-li-desc">' + getTimeString(now.getTime() - trail.pubDate.getTime()) + '</p>';
          li += "</li>";
          $list.append(li);
-      });
+      }
       $list.listview('refresh');
    });
 });
