09868b8ba9a0c906e7b49261f3f3331e6c3db5c9
unknown <gerritsj@.simon.com>
2012-03-20 08:44:50 -0400

    forgot to start server in app.js

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ var tako = require('tako'), request = require('request'), path = require('path')
 //app.route('/index.html').file(path.join(__dirname, 'static/index.html'));
 //app.route('/').file(path.join(__dirname, 'static/index.html'));
 app.route('/').html('<h1>hello</h1>');
+
+console.log(process.env.PORT || 9898);
+app.httpServer.listen(process.env.PORT || 9898);
