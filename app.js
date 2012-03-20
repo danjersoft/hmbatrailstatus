0ba825298f15763d06ff0ca8e7d4908721dca7e1
unknown <gerritsj@.simon.com>
2012-03-20 08:40:01 -0400

    trying a more simple app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var tako = require('tako'), request = require('request'), path = require('path'), app = tako();
 
-app.route('/index.html').file(path.join(__dirname, 'static/index.html'));
-app.route('/').file(path.join(__dirname, 'static/index.html'));
+//app.route('/index.html').file(path.join(__dirname, 'static/index.html'));
+//app.route('/').file(path.join(__dirname, 'static/index.html'));
+app.route('/').html('<h1>hello</h1>');
