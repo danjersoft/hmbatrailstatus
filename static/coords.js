86f0ba1ab0f379c03ac5e90276ba15449113479a
Jeremy Gerrits <danjersoftware@gmail.com>
2012-03-21 21:56:33 -0400

    all available trails with lat and longs

---
+++ static/coords.js
@@ -0,0 +1,72 @@
+[
+   {
+      "trail": "Brown County State Park",
+      "lat": 39.190838,
+      "long": -86.217957
+   },
+   {
+      "trail": "Ferdinand State Forest",
+      "lat": 38.259008,
+      "long": -86.789246
+   },
+   {
+      "trail": "Town Run Trail Park",
+      "lat": 39.92571,
+      "long": -86.076768
+   },
+   {
+      "trail": "Muscatatuck",
+      "lat": 38.988652,
+      "long": -85.622571
+   },
+   {
+      "trail": "Rangeline Nature Preserve",
+      "lat": 40.099823,
+      "long": -85.634522
+   },
+   {
+      "trail": "O'Bannon Woods State Park",
+      "lat": 38.199204,
+      "long": -86.268511
+   },
+   {
+      "trail": "Avon/Washington Park",
+      "lat": 39.760857,
+      "long": -86.423296
+   },
+   {
+      "trail": "Fort Harrison State Park",
+      "lat": 39.860769,
+      "long": -86.018915
+   },
+   {
+      "trail": "French Lick",
+      "lat": 38.557646,
+      "long": -86.626189
+   },
+   {
+      "trail": "Harmonie State Park",
+      "lat": 38.046266,
+      "long": -87.959204
+   },
+   {
+      "trail": "Southwestway Park",
+      "lat": 39.655762,
+      "long": -86.241775
+   },
+   {
+      "trail": "Versailles State Park",
+      "lat": 39.08194,
+      "long": -85.233264
+   },
+   {
+      "trail": "Wapehani",
+      "lat": 39.143541,
+      "long": -86.568918
+   },
+   {
+      "trail": "Westwood Park",
+      "lat": 39.899317,
+      "long": -85.437541
+   }
+]
